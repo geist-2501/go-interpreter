3bf6bb115eafdf7803306624d21e228136ed39d7
geist-2501 <blaironbute@gmail.com>
2024-05-19 20:38:16 +0100

    added full lexer support

--- token/token.go
+++ token/token.go
@@ -3,31 +3,63 @@ package token
 type TokenType string
 
 type Token struct {
-    Type TokenType
-    Literal string
+	Type    TokenType
+	Literal string
 }
 
 const (
-    ILLEGAL = "ILLEGAL"
-    EOF = "EOF"
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
 
-    // Identifiers
-    IDENT = "IDENT"
-    INT = "INT"
+	// Identifiers
+	IDENT = "IDENT"
+	INT   = "INT"
 
-    // Operators
-    ASSIGN = "="
-    PLUS = "+"
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+    MINUS  = "-"
+    BANG   = "!"
+    ASTERISK = "*"
+    SLASH  = "/"
 
-    // Delimiters
-    COMMA = ","
-    SEMICOLON = ";"
-    LPAREN = "("
-    RPAREN = ")"
-    LBRACE = "{"
-    RBRACE = "}"
+    LT = "<"
+    GT = ">"
+    EQ = "=="
+    NOT_EQ = "!="
 
-    // Keywords
-    FUNCTION = "FUNCTION"
-    LET = "LET"
+	// Delimiters 
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+    TRUE = "TRUE"
+    FALSE = "FALSE"
+    IF = "IF"
+    ELSE = "ELSE"
+    RETURN = "RETURN"
 )
+
+var keywords = map[string]TokenType{
+    "fn": FUNCTION,
+    "let": LET,
+    "true": TRUE,
+    "false": FALSE,
+    "if": IF,
+    "else": ELSE,
+    "return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+    if tok, ok := keywords[ident]; ok {
+        return tok;
+    }
+
+    return IDENT
+}
