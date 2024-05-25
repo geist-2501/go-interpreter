df7a71a2a9fd8dc96ea45cee7e24d04d5da856ac
geist-2501 <blaironbute@gmail.com>
2024-05-25 13:21:21 +0100

    added basic AST

---
+++ ast/ast.go
@@ -0,0 +1,44 @@
+package ast
+
+import "go/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type Identifier struct {
+    Token token.Token
+    Value string
+}
+
+type LetStatement struct {
+	Token token.Token
+    Name *Identifier
+    Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
